92f296e85a543b493048173b254fc3c247498a07
Jan Bauer Nielsen <jbn@dbc.dk>
2019-09-18 20:38:09 +0200

    Add DBC utility class

---
+++ README.md
@@ -0,0 +1,21 @@
+libcore
+=======
+
+A Java library containing core utils for DBC.
+
+### contents
+
+ * DBC
+ 
+> A static utility class with knowledge of DBC agency IDs
+
+### development
+
+**Requirements**
+
+To build this project JDK 1.8 or higher and Apache Maven is required.
+
+### License
+
+Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3.
+See license text in LICENSE.txt
---
+++ src/main/java/dk/dbc/libcore/DBC.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.libcore;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class DBC {
+    /**
+     * DBC agency ID
+     */
+    public static Agency agency = new Agency();
+
+    private static final Set<String> PORTFOLIO = Stream.of(
+            "870970", "870971", "870974", "870979",
+            "190002", "190004"
+    ).collect(Collectors.toSet());
+
+    /**
+     * @param agencyId agency ID to test for DBC governance
+     * @return true if DBC governs the specified agency, false if not
+     */
+    public static boolean governs(String agencyId) {
+        return PORTFOLIO.contains(agencyId);
+    }
+
+    /**
+     * @param agencyId agency ID to test for DBC governance
+     * @return true if DBC governs the specified agency, false if not
+     */
+    public static boolean governs(int agencyId) {
+        return PORTFOLIO.contains(Integer.toString(agencyId));
+    }
+
+    private DBC() {}
+
+    public static final class Agency {
+        private Agency() {}
+
+        public int toInt() {
+            return 191919;
+        }
+
+        @Override
+        public String toString() {
+            return "191919";
+        }
+    }
+}
